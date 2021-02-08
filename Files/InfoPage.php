4ec24a569af2a6fcb26b190da9e3310dce778a20
Leo <50770485+LuckyLuke77@users.noreply.github.com>
2021-02-08 17:08:12 +0200

    Add files via upload

---
+++ Files/InfoPage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Information Page</title>
+    <style>
+        <?php 
+         include 'Style.css'; 
+        ?>
+    </style>
+</head>
+<body>
+    <h2>Welcome to the information page</h2>
+    <p>Information</p>
+    <p><a href="HomePage.php">Go back</a><p>
+</ul>
+</body>
+</html>
