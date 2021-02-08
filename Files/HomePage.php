3e21a6ea366b8f4049a47908939fcd20bfc48289
Leo <50770485+LuckyLuke77@users.noreply.github.com>
2021-02-08 17:07:36 +0200

    Create HomePage.php
    The Home page

---
+++ Files/HomePage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home Page</title>
+    <style>
+        <?php 
+         include 'Style.css'; 
+        ?>
+    </style>
+</head>
+<body>
+    <h2>Welcome to the home page</h2>
+    <ul>
+        <li><a href="https://youtu.be/dQw4w9WgXcQ">Sample Link 1</a></li>
+        <li><a href="InfoPage.php">Sample Link 2</a></li>
+        <li>Sample link 3</li>
+</ul>
+</body>
+</html>
+    
