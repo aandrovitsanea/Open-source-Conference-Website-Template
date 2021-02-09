b7c0efb0b4702949e5c8c71d9bf6dddb0d83c5df
Leo <50770485+LuckyLuke77@users.noreply.github.com>
2021-02-09 16:05:00 +0200

    basic layout

--- Files/HomePage.php
+++ Files/HomePage.php
@@ -9,12 +9,32 @@
     </style>
 </head>
 <body>
-    <h2>Welcome to the home page</h2>
-    <ul>
-        <li><a href="https://youtu.be/dQw4w9WgXcQ">Sample Link 1</a></li>
-        <li><a href="InfoPage.php">Sample Link 2</a></li>
-        <li>Sample link 3</li>
-</ul>
+<!------------------------------------------ LOGO -------------------------------------------------------->
+    <?php 
+        echo "<img id='logo' src='https://cdn.discordapp.com/attachments/808029496614715482/808681331424755752/logo2.png'>"; 
+    ?><br>
+<!---------------------------------------- MENU BAR ------------------------------------------------------>
+    <div id="menu">
+        <ul id="NavBar">
+            <li class="fillers">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+            <li><a href="https://youtu.be/dQw4w9WgXcQ">Home</a></li>
+            <li><a href="InfoPage.php">About</a></li>
+            <li><a href="InfoPage.php">Location</a></li>
+            <li><a href="InfoPage.php">Jury</a></li>
+            <li><a href="InfoPage.php">Program</a></li>
+            <li><a href="InfoPage.php">Subscribe</a></li>
+            <li class="fillers">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+        </ul>
+    </div>
+    <br>
+    <div id="main">
+        <h2>Welcome to the home page</h2>
+        <p>
+        SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT 
+        SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT 
+        SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT SAMPLE TEXT 
+        </p>
+    </div>
 </body>
 </html>
     
