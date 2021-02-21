d7b697c709c6b8d6a7adca1d507266e42fb76640
aandrovitsanea <anna.androvitsanea@th-luebeck.de>
2021-02-21 16:28:02 +0200

    add .sql file and delete all reduntant data

--- Files/conference_db.sql
+++ Files/conference_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 19, 2021 at 11:18 PM
+-- Generation Time: Feb 21, 2021 at 03:27 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.2.34
 
@@ -31,20 +31,21 @@ CREATE TABLE `conference_db` (
   `fname` varchar(100) NOT NULL,
   `lname` varchar(100) NOT NULL,
   `dob` date NOT NULL,
-  `gender` varchar(100) NOT NULL,
+  `gender` varchar(20) NOT NULL,
   `country` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `telephone` varchar(20) NOT NULL,
-  `password` varchar(100) NOT NULL
+  `telephone` varchar(10) NOT NULL,
+  `username` varchar(8) NOT NULL,
+  `password` varchar(8) NOT NULL,
+  `consent` varchar(3) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `conference_db`
 --
 
-INSERT INTO `conference_db` (`fname`, `lname`, `dob`, `gender`, `country`, `email`, `telephone`, `password`) VALUES
-('Anna', 'Androvitsanea', '2007-12-11', 'non_bin', 'Greece', 'anna@mail.com', '6949465821', 'zz'),
-('thomas', 'Androvitsanea', '2007-10-11', 'non_bin', 'Gambia', 'thomas@mail.com', '6949465821', 'ff');
+INSERT INTO `conference_db` (`fname`, `lname`, `dob`, `gender`, `country`, `email`, `telephone`, `username`, `password`, `consent`) VALUES
+('Thomas', 'Anderson', '1980-02-16', 'Mr', 'Cyprus', 'anderson@mail.com', '2105555555', 'anderson', 'anderson', 'yes');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
