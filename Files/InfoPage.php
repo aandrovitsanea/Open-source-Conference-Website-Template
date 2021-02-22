94c85d040401993aaf5f95afdea1810fc5fd573d
aandrovitsanea <anna.androvitsanea@th-luebeck.de>
2021-02-23 01:38:37 +0200

    add video on web technologies to infos

--- Files/InfoPage.php
+++ Files/InfoPage.php
@@ -22,6 +22,12 @@
         
         <p>It focuses on real world applications; therefore authors should highlight the benefits of Web Information Systems and Technologies for industry and services, in addition to academic applications. Ideas on how to solve business problems using web-based information systems and technologies, will be discussed at the conference.</p>
         
+        <p>Here are some "spoilers" of what our conference is all about: Wonder what technologies and trends will dominate the web development in 2020 and 2025? Watch this interesting talk on the topic.</p>
+        
+        <p style = 'text-align: center;'><iframe width="560" height="315" src="https://www.youtube.com/embed/24tQRwIRP_w" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" title="W3Schools Free Online Web Tutorials" allowfullscreen></iframe></p>
+        
+        
+        
     </div>
 </body>
 </html>
