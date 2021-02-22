9fb366b64dbcc90ace1d28b2725d858d5fdd8341
aandrovitsanea <anna.androvitsanea@th-luebeck.de>
2021-02-23 01:39:34 +0200

    change venue to Syntagma - add map - add section accomodation

--- Files/LocationPage.php
+++ Files/LocationPage.php
@@ -26,6 +26,7 @@
                         </button>
                     <div class="dropdown-content">
                         <a href="#loc">Venue</a>
+                        <a href="#stay">Accommodation</a>
                         <a href="#guide">City guide</a>
                         <a href="#other_conf">More conferences</a>
                     </div></li>
@@ -40,18 +41,51 @@
                 
         <h2 id="loc">Where To Find Us</h2>
         <p>
-            <b>The International Conference on Web Technologies</b> is taking place at <i>Odyssea Androutsou 3, Marousi, Athens</i>
+            <b>The International Conference on Web Technologies</b> is taking place in the heart of the busy city of Athens, at Syntagma Square, <i>Leoforos Vasilisis Amalias 133, Athina 105 57</i>.
         </p>
-        <h4>Instructions</h4>
         <p>
-            As seen in the map below, <i>Odyssea Androutsou 3</i> is a 2-minute walk from the nearest <br> metro-station; <a href="https://en.wikipedia.org/wiki/Marousi_metro_station" style="text-decoration:none;color:blue;">Marousi</a>
+            The venue is a 2-minute walk from the nearest metro-station; <a href="https://en.wikipedia.org/wiki/Syntagma_metro_station" style="text-decoration:none;color:blue;">Syntagma</a>.
         </p>
 <!----------------------- EMBED GOOGLE MAP FROM: https://www.embedgooglemap.net/ ------------------------->
         <div id="map">
-            <div class="mapouter"><div class="gmap_canvas"><iframe width="350" height="350" id="gmap_canvas" src="https://maps.google.com/maps?q=odyssea%20androutsou%203,%20marousi&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://soap2day-to.com">soap2day</a><br><style>.mapouter{position:relative;text-align:right;height:350px;width:350px;}</style><a href="https://www.embedgooglemap.net">embedgooglemap.net</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:350px;width:350px;}</style></div></div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1111.954170666153!2d23.735067268376994!3d37.97523107488494!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14a1bd3edda00197%3A0xc496fdfe023ef242!2sMonument%20of%20the%20Unknown%20Soldier!5e0!3m2!1sen!2sgr!4v1614035008681!5m2!1sen!2sgr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
         
 <!----------------------------------- CITY GUIDE --------------------------------------------------->        
+        <h2 id="stay">Where To Stay</h2>
+        <p>
+            There is a great variety of hotel in the city of Athens. For options 1 - 6 you can get a discount, upon mentioning of our conference. <br> <b>Hurry up and book upfront!</b>
+        </p>
+        
+        <ol>            
+            <li><a href="http://lycabettus.athenshotels.it" class="external">St. George Lycabettus Boutique Hotel</a></li>
+            <li><a href="http://newhotel.athenshotels.it" class="external">New Hotel</a></li>
+            <li><a href="http://royalolympichotel.athenshotels.it" class="external">Royal Olympic Hotel</a></li>
+            <li><a href="http://electrapalace.athenshotels.it" class="external">Electra Palace Athens</a></li>
+            <li><a href="http://semiramis.athenshotels.it" class="external">Semiramis Hotel</a></li>
+            <li><a href="http://divanicaravel.athenshotels.it" class="external">Divani Caravel</a></li>
+            <li><a href="?param[lang]=en&amp;param[idmarea]=1&amp;param[loc_country]=GRC&amp;param[loc]=HIATH&amp;param[idhtl]=1875127&amp;action=form_hotel_description">Sofitel Athens Airport</a></li>
+            <li><a href="http://classicalkinggeorge.athenshotels.it" class="external">King George, a Luxury Collection Hotel, Athens</a></li>
+            <li><a href="http://crowneplaza.athenshotels.it" class="external">Crowne Plaza Athens City Centre</a></li>
+            <li><a href="?param[lang]=en&amp;param[idmarea]=1&amp;param[loc_country]=GRC&amp;param[loc]=HIATH&amp;param[idhtl]=1919048&amp;action=form_hotel_description">Hotel Grande Bretagne, a Luxury Collection Hotel, Athens</a></li>
+            <li><a href="?param[lang]=en&amp;param[idmarea]=1&amp;param[loc_country]=GRC&amp;param[loc]=HIATH&amp;param[idhtl]=124988&amp;action=form_hotel_description">Athens Marriott Hotel</a></li>
+            <li><a href="?param[lang]=en&amp;param[idmarea]=1&amp;param[loc_country]=GRC&amp;param[loc]=HIATH&amp;param[idhtl]=2048523&amp;action=form_hotel_description">Electra Metropolis Athens</a></li>
+            <li><a href="?param[lang]=en&amp;param[idmarea]=1&amp;param[loc_country]=GRC&amp;param[loc]=HIATH&amp;param[idhtl]=124254&amp;action=form_hotel_description">Hilton Athens</a></li>
+            <li><a href="?param[lang]=en&amp;param[idmarea]=1&amp;param[loc_country]=GRC&amp;param[loc]=HIATH&amp;param[idhtl]=2017185&amp;action=form_hotel_description">NJV Athens Plaza Hotel</a></li>
+            <li><a href="?param[lang]=en&amp;param[idmarea]=1&amp;param[loc_country]=GRC&amp;param[loc]=HIATH&amp;param[idhtl]=2019174&amp;action=form_hotel_description">Wyndham Grand Athens</a></li>
+            <li><a href="?param[lang]=en&amp;param[idmarea]=1&amp;param[loc_country]=GRC&amp;param[loc]=HIATH&amp;param[idhtl]=139223&amp;action=form_hotel_description">Athenaeum InterContinental</a></li>
+            <li><a href="?param[lang]=en&amp;param[idmarea]=1&amp;param[loc_country]=GRC&amp;param[loc]=HIATH&amp;param[idhtl]=641226&amp;action=form_hotel_description">AthensWas</a></li>
+            <li><a href="?param[lang]=en&amp;param[idmarea]=1&amp;param[loc_country]=GRC&amp;param[loc]=HIATH&amp;param[idhtl]=1892613&amp;action=form_hotel_description">COCO-MAT Athens BC</a></li>
+            <li><a href="http://divaniacropolis.athenshotels.it" class="external">Divani Palace Acropolis</a></li>
+            <li><a href="?param[lang]=en&amp;param[idmarea]=1&amp;param[loc_country]=GRC&amp;param[loc]=HIATH&amp;param[idhtl]=1847136&amp;action=form_hotel_description">Glyfada Riviera Hotel</a></li>
+            <li><a href="?param[lang]=en&amp;param[idmarea]=1&amp;param[loc_country]=GRC&amp;param[loc]=HIATH&amp;param[idhtl]=1370224&amp;action=form_hotel_description">Grand Hyatt Athens</a></li>
+            <li><a href="?param[lang]=en&amp;param[idmarea]=1&amp;param[loc_country]=GRC&amp;param[loc]=HIATH&amp;param[idhtl]=2019839&amp;action=form_hotel_description">Grecotel Pallas Athena</a></li>
+            <li><a href="?param[lang]=en&amp;param[idmarea]=1&amp;param[loc_country]=GRC&amp;param[loc]=HIATH&amp;param[idhtl]=1899262&amp;action=form_hotel_description">Life Gallery</a></li>
+            <li><a href="?param[lang]=en&amp;param[idmarea]=1&amp;param[loc_country]=GRC&amp;param[loc]=HIATH&amp;param[idhtl]=1370950&amp;action=form_hotel_description">Perianth Hotel</a></li>
+            <li><a href="?param[lang]=en&amp;param[idmarea]=1&amp;param[loc_country]=GRC&amp;param[loc]=HIATH&amp;param[idhtl]=1952673&amp;action=form_hotel_description">The Pinnacle Athens</a></li>
+            <li><a href="?param[lang]=en&amp;param[idmarea]=1&amp;param[loc_country]=GRC&amp;param[loc]=HIATH&amp;param[idhtl]=1951467&amp;action=form_hotel_description">Theoxenia Palace Hotel</a></li>
+            <br class="clear">
+        </ol> 
         
         <h2 id="guide">City guide</h2>
         <p>
