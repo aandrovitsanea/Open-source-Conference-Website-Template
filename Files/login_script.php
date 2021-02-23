ea0b7658874c09d0799d0b3005a2a287d462bc6d
aandrovitsanea <anna.androvitsanea@th-luebeck.de>
2021-02-23 22:11:00 +0200

    print please sign up in the body

--- Files/login_script.php
+++ Files/login_script.php
@@ -1,25 +1,4 @@
-<?php
 
-include("database_connection.php"); // import database connection
-?>
-
-    <?php
-    $password = mysqli_query($connection, "SELECT `password` FROM `conference_db` WHERE `email` = '".$_POST['email']."';"); // extract the hash saved as password in the database
-
-    $row = mysqli_fetch_assoc($password); // fetch the result of the query
-
-    // check if I get a result (not null) and the given hash created from the posted password matches the hash stored in the database
-    
-    if($row != null && password_verify($_POST['password'], $row['password'])){
-        session_start(); // start session
-        $_SESSION["email"] = $_POST['email']; // store email to session; use as marker to check if user is logged in 
-        header("location:Program.php"); // redirect to the Program page
-    }else{
-        echo "<h4 style= 'color:red;';>You are not signed up to our conference.<br><br></h4>";
-        echo "<h4>Please subscribe <a href='subscribe_form.php'>here</a>.</h4>";
-    }
-
-    ?> 
     
 <!DOCTYPE html>
 <html>
@@ -55,8 +34,29 @@ include("database_connection.php"); // import database connection
     <br>
     
     <div id="main">
+        <?php
 
+        include("database_connection.php"); // import database connection
+        ?>
 
-            </div>
-    </body>
+        <?php
+        $password = mysqli_query($connection, "SELECT `password` FROM `conference_db` WHERE `email` = '".$_POST['email']."';"); // extract the hash saved as password in the database
+
+        $row = mysqli_fetch_assoc($password); // fetch the result of the query
+
+        // check if I get a result (not null) and the given hash created from the posted password matches the hash stored in the database
+        
+        if($row != null && password_verify($_POST['password'], $row['password'])){
+            session_start(); // start session
+            $_SESSION["email"] = $_POST['email']; // store email to session; use as marker to check if user is logged in 
+            header("location:Program.php"); // redirect to the Program page
+        }else{
+            echo "<h4 style= 'color:red;';>You are not signed up to our conference.<br><br></h4>";
+            echo "<h4>Please subscribe <a href='subscribe_form.php'>here</a>.</h4>";
+        }
+
+        ?> 
+
+    </div>
+  </body>
 </html>
